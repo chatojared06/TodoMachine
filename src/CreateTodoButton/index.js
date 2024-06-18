e3607c46fb0a415d72964274ad2c6163ef359663
Jared <jaredozono@gmail.com>
2024-06-18 15:02:04 -0700

    guadando cambios pendientes

--- src/CreateTodoButton/index.js
+++ src/CreateTodoButton/index.js
@@ -1,18 +1,16 @@
 import './CreateTodoButton.css';
 
 function CreateTodoButton({ setOpenModal, openModal }) {
-   return (
-        <button 
-        className='mi-boton'
-        onClick={(event) => {
-            openModal ? 
-            setOpenModal(false) :
-            setOpenModal(true) 
-
-        }}>
-            +
-        </button>
-    );
+  return (
+    <button
+      className='mi-boton'
+      onClick={(event) => {
+        openModal ? setOpenModal(false) : setOpenModal(true);
+      }}
+    >
+      +
+    </button>
+  );
 }
 
 export { CreateTodoButton };
